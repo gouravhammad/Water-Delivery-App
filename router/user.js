462194911506fe0340212dec57930bef2720a64e
Gourav Hammad <gouravhammad477@gmail.com>
2019-12-28 10:34:42 +0530

    Done User Update

--- router/user.js
+++ router/user.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const { check, validationResult } = require('express-validator');
 const router = express.Router()
 const connection = require('../utility/mysqlConn')
 
@@ -20,6 +21,24 @@ router.use('/',function(req,res,next){
         {
             next()
         } 
+    }
+    catch(e)
+    {
+        res.redirect('/')
+    }
+})
+
+router.get('/logout',function(req,res){ 
+    try
+    {
+         req.session.mobileno = null
+
+         res.render('Alert',{
+            type:"success",
+            title:"Logged Out",
+            text:"You have been logged out",
+            link:"/"
+        })
     }
     catch(e)
     {
@@ -115,16 +134,22 @@ router.post('/addToCart',function(req,res){
     }
 })
 
-router.get('/logout',function(req,res){ 
+router.get('/myAccount',function(req,res){
     try
     {
-         req.session.mobileno = null
+        var sql = "select * from user where mobileno = ?"
+        x = [[user.mobileno]]
 
-         res.render('Alert',{
-            type:"success",
-            title:"Logged Out",
-            text:"You have been logged out",
-            link:"/"
+        connection.query(sql,[x],function(err,result){
+            if(err) throw err
+            if(result.length == 1)
+            {
+                res.render('MyAccount',{result})
+            }
+            else
+            {
+                console.log("Problem myAccount")
+            }
         })
     }
     catch(e)
@@ -132,5 +157,106 @@ router.get('/logout',function(req,res){
         res.redirect('/')
     }
 })
+
+router.get('/updateUser',function(req,res){
+    try
+    {
+        var sql = "select * from user where mobileno = ?"
+        x = [[user.mobileno]]
+
+        connection.query(sql,[x],function(err,result){
+            if(err) throw err
+            if(result.length == 1)
+            {
+                res.render('UpdateUser',{user:result[0]})
+            }
+            else
+            {
+                console.log("Problem myAccount")
+            }
+        })
+    }
+    catch(e)
+    {
+        res.redirect('/')
+    }
+})
+
+router.post('/saveChangesUser', [
+    check('email','Email must be of Min 5 and Max 30 length').trim().isEmail().isLength({ min: 5 , max: 30}),
+    check('username','Must be of Min 5 and Max 20 length').trim().isLength({ min: 5 , max: 20}),
+    check('password','Must be of Min 8 and Max 20 length').trim().isLength({ min: 8 , max: 20}),
+    check('address','Must be of Min 5 and Max 20 length').trim().isLength({ min: 5 , max: 20})
+  ], (req, res) => {
+
+    try
+    {
+        const errors = validationResult(req);
+        
+        user = {
+            email: req.body.email,
+            username: req.body.username,
+            address: req.body.address,
+            password: req.body.password,
+            mobileno: req.body.mobileno
+        }
+
+        error = {nameError:null, passwordError:null, emailError:null, addressError:null}
+
+        for(i = 0; i < errors.errors.length; i++)
+        {
+            name =  errors.errors[i].param
+
+            if(name === 'username')
+            {
+                error.nameError = errors.errors[i].msg
+            }
+            else if(name === 'email')
+            {
+                error.emailError = errors.errors[i].msg
+            }
+            else if(name === 'password')
+            {
+                error.passwordError = errors.errors[i].msg
+            }
+            else
+            {
+                error.addressError = errors.errors[i].msg
+            }
+        }
+
+        if (!errors.isEmpty())
+        {
+            res.render('UpdateUser',{user:user,error:error})
+        }
+        else
+        {
+            var sql = "update user set username = '" + user.username + "', email = '" + user.email + "', address = '" + user.address + "', password = '" + user.password + "' where mobileno = " + user.mobileno
+          
+            console.log(sql)
+            connection.query(sql,function(err,result){
+                if(err) throw err
+                if(result.affectedRows == 1)
+                {
+                    res.render('Alert',{
+                        type:"success",
+                        title:"Account Updated",
+                        text:"You have successfully updated your account",
+                        link:"/user/myAccount"
+                    })   
+                }
+                else
+                {
+                    console.log("Error in Updating")
+                }
+            }) 
+        }
+    }
+    catch(e)
+    {
+        res.redirect('/')
+    }
+})
+
 
 module.exports = router
